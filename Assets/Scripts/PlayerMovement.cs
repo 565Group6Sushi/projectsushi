2e543702977ecba8fcd0e2e904016f691dbabf15
Raikuman <40256283+Raikuman@users.noreply.github.com>
2022-03-22 13:37:30 -0700

    Reworked Movement Script
    Cleaned up movement and added raycasting to check grounding

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,43 +4,75 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     private CharacterController characterController;
-    private float defaultSpeed = 2.0f, jumpHeight = 1.0f, gravity = -9.81f;
-    private bool grounded;
-    private Vector3 playerVelocity;
+    private float gravity = -9.81f;
+
+    public float playerSpeed = 2.0f, jumpHeight = 1.0f;
+    public bool isGrounded;
+    public float turnSmoothTime = 0.15f;
+
+    Vector3 velocity;
+    float turnSmoothVelocity;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        characterController = gameObject.AddComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        grounded = characterController.isGrounded;
-        if (grounded && playerVelocity.y < 0)
+        // Check Grounded
+        CheckGrounded();
+
+        if (isGrounded && velocity.y < 0)
         {
-            playerVelocity.y = 0f;
+            velocity.y = -2f;
         }
 
-        // Calculate movement from input
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(movement * Time.deltaTime * defaultSpeed);
-
-        if (movement != Vector3.zero)
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            gameObject.transform.forward = movement;
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
 
-        if (Input.GetButtonDown("Jump") && grounded)
+        // Gravity
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+
+        // Movement
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if (movement.magnitude >= 0.1f)
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+            // Handle Player Direction
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // Handle Movement
+            Vector3 movementDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(movementDirection.normalized * playerSpeed * Time.deltaTime);
         }
+    }
 
-        playerVelocity.y += gravity * Time.deltaTime;
-        characterController.Move(playerVelocity * Time.deltaTime);
+    void CheckGrounded()
+    {
+        RaycastHit hit;
+        float distance = 1f;
+        Vector3 direction = new Vector3(0, -1);
 
+        if (Physics.Raycast(transform.position, direction, out hit, distance))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 }
