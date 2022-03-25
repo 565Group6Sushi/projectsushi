4b51a922f000b9603ba44b908a2268332850a657
nikkoaustria <nikkoaustria6@gmail.com>
2022-03-25 16:54:13 -0700

    added sushi character, new movement script, and camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+
+	public float smoothSpeed = 10f;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(desiredPosition, target.position, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition; 
+    }
+}
