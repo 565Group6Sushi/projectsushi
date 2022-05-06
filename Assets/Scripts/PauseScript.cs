94a927fc6733d31c45b69f4c4aff1b30270f6f9e
davidfai <faizidavid1@gmail.com>
2022-05-06 14:42:20 -0700

    Merge branch 'develop/assets-and-level-3' of https://github.com/565Group6Sushi/projectsushi into develop/assets-and-level-3

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool GameIsPaused;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    private void Start()
+    {
+        GameIsPaused = false;
+        Resume();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Escape Was Pushed");
+            if (GameIsPaused)
+            {
+                Resume();
+
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        //Time.timeScale = 1f;
+        GameIsPaused = false;
+        
+    }
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+       // Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu() {
+      //  SceneManager.LoadScene("Menu");
+    }
+   
+}
+// Pause Script made from help of https://www.youtube.com/watch?v=JivuXdrIHK0 
